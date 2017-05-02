3c20bf061f8f9ca97171dbaf0ae418fb41f1050b
tongshuo <tongshuo@xin.com>
2017-05-02 21:10:21 +0800

    增加js

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,8 @@ var router = require('koa-router')();
 
 module.exports = router.get('/',async function (ctx, next) {
     ctx.state = {
-        style:['stylesheets/banner.css']
+        style:['stylesheets/banner.css'],
+        script:['scripts/home.js']
     };
 
     await ctx.render('index', {
