84fe8cb6766a2b400c33bde4216553e8ebbdecbf
tongshuo <tongshuo@xin.com>
2017-05-03 22:08:13 +0800

    banner

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,26 +1,16 @@
 console.log('index主页');
 
 
-/*banner图高度*/
-var winHeight = 0;
-function findDimensions() //函数：获取尺寸
-{
-    //获取窗口高度
-    if (window.innerHeight)
-        winHeight = window.innerHeight;
-    else if ((document.body) && (document.body.clientHeight))
-        winHeight = document.body.clientHeight;
-    //通过深入Document内部对body进行检测，获取窗口大小
-    if (document.documentElement && document.documentElement.clientHeight && document.documentElement.clientWidth)
-    {
-        winHeight = document.documentElement.clientHeight;
-        winWidth = document.documentElement.clientWidth;
-    }
-    // 结果改变banner的高度
-    $('#banner').css('height',winHeight+'px');
-}
-findDimensions();
-//调用函数，获取数值
-window.onresize=findDimensions;
-
-
+var swiper = new Swiper('.swiper-container', {
+        initialSlide:2,
+        loop:true,
+        speed:1000,
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        spaceBetween: 0,
+        centeredSlides: true,
+        autoplay: 5000,
+        autoplayDisableOnInteraction: false
+    });
--- routes/home.js
+++ routes/home.js
@@ -3,8 +3,8 @@ var router = require('koa-router')();
 
 module.exports = router.get('/',async function (ctx, next) {
     ctx.state = {
-        style:['stylesheets/banner.css'],
-        script:['scripts/index.js']
+        style:['stylesheets/banner.css','stylesheets/common/swiper-3.4.2.min.css'],
+        script:['scripts/common/swiper.min.js','scripts/index.js']
     };
 
     await ctx.render('index', {
